9222d6710690a1eb8e9d4749b9c154d84894f465
hiefs <113737497+hiefs@users.noreply.github.com>
2026-07-13 18:55:11 -0400

    start anew

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,18 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://hiefs.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.fontsource(),
-			name: "Oswald",
-			cssVariable: "--font-oswald",
-		},
-		{
-			provider: fontProviders.fontsource(),
-			name: "JetBrains Mono",
-			cssVariable: "--font-jet-brains",
-		},
-		{
-			provider: fontProviders.fontsource(),
-			name: "Bebas Neue",
-			cssVariable: "--font-bebas-neue",
-		}
-	],
+  site: "https://hiefs.com",
+  integrations: [mdx(), sitemap()],
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "JetBrains Mono",
+      cssVariable: "--font-jet-brains",
+    },
+  ],
 });
