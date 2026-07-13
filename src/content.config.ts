97e2a4ad985a86f3e36dd8b301474c3009c2ef9d
hiefs <113737497+hiefs@users.noreply.github.com>
2026-07-13 18:55:23 -0400

    plus these oopsie

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,35 +1,35 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+// import { defineCollection } from "astro:content";
+// import { glob } from "astro/loaders";
+// import { z } from "astro/zod";
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: () =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional()
-		}),
-});
+// const blog = defineCollection({
+// 	// Load Markdown and MDX files in the `src/content/blog/` directory.
+// 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+// 	// Type-check frontmatter using a schema
+// 	schema: () =>
+// 		z.object({
+// 			title: z.string(),
+// 			description: z.string(),
+// 			// Transform string to Date object
+// 			pubDate: z.coerce.date(),
+// 			updatedDate: z.coerce.date().optional()
+// 		}),
+// });
 
-const games = defineCollection({
-	// Load Markdown and MDX files in the `src/content/games/` directory.
-	loader: glob({ base: './src/content/games', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-			heroImage2: z.optional(image())
-		}),
-});
+// const games = defineCollection({
+// 	// Load Markdown and MDX files in the `src/content/games/` directory.
+// 	loader: glob({ base: './src/content/games', pattern: '**/*.{md,mdx}' }),
+// 	// Type-check frontmatter using a schema
+// 	schema: ({ image }) =>
+// 		z.object({
+// 			title: z.string(),
+// 			description: z.string(),
+// 			// Transform string to Date object
+// 			pubDate: z.coerce.date(),
+// 			updatedDate: z.coerce.date().optional(),
+// 			heroImage: z.optional(image()),
+// 			heroImage2: z.optional(image())
+// 		}),
+// });
 
-export const collections = { blog, games };
+export const collections = {};
